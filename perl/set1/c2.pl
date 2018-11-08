d3afee1db3f6345f65a14db84f43af0d5400f81e
Will Cass <will_cass@hotmail.co.uk>
2018-11-08 17:50:16 +0000

    perl - cleaning up the code

--- perl/set1/c2.pl
+++ perl/set1/c2.pl
@@ -4,11 +4,7 @@ use warnings;
 
 my $input = pack "H*" ,"1c0111001f010100061a024b53535009181c";
 my $key = pack "H*" ,"686974207468652062756c6c277320657965";
-#my $result = substr($key,0,length($key))^substr($input,0,length($input));
 my $result = $key^$input;
-$result = unpack "H*", $result;
-#for $i (0..length($input)-1){
-#	$result += substr()
-#
 
+$result = unpack "H*", $result;
 print "[*] - $result";
