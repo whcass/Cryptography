3a443cd27b622f67142872dca1513eeea31f6ae4
Will Cass <will_cass@hotmail.co.uk>
2018-11-10 02:57:37 +0000

    Challenge 3 done

--- perl/set2/c3.pl
+++ perl/set2/c3.pl
@@ -19,11 +19,109 @@ Detect the block cipher mode the function is using each time. You should end up 
 =cut
 use strict;
 use warnings;
+use MIME::Base64;
+use Crypt::OpenSSL::AES;
+use Crypt::Mode::ECB;
+use Data::Dumper;
 my $file = 'resources/bobross.txt';
 
+# my $plainText;
+# {
+#     local $/;
+#     open my $fh, '<', $file or die "can't open $file: $!";
+#     $plainText = <$fh>;
+# }
+my ($ecb, $cbc, $found) = (0,0,0);
+my $plainText = 'x' x 50;
+for (1..1000){
+    my $cipher = encryption_oracle($plainText);
+    my $blocks;
+    for(my $offset = 0; $offset < length($cipher); $offset += 16){
+        my $block = substr $cipher, $offset, 16;
+        if($blocks->{$block}++){
+            $found++;
+            last;
+        }
+    }
+}
+print "[ecb] $ecb\n";
+print "[cbc] $cbc\n";
+print "[found] $found\n";
+
+sub encryption_oracle {
+    my ($plainText) = @_;
+    $plainText = pad_plaintext($plainText);
+    my $encryptType = int(rand(2));
+    my $key = generate_aes_key();
+    #my $key = "YELLOW SUBMARINE";
+    my $encrypt = Crypt::OpenSSL::AES->new($key);
+    my $cipher;
+    if($encryptType == 0){
+        my $iv = generate_aes_key();
+        $cipher = cbc_encrypt($plainText,$iv,$encrypt);
+        #print "[mode] CBC\n";
+        $cbc++;
+    }else{
+        $cipher = ecb_encrypt($plainText,$encrypt);
+        $ecb++;
+        #print "[mode] ECB\n";
+    }
+
+    return $cipher;
+}
+
+sub pad_plaintext {
+    my ($plainText) = @_;
+    my $minimum = 5;
+    my $maximum = 10;
+    my $beforePad = pad("",rand_between(5,10));
+    my $afterPad = pad("",rand_between(5,10));
+
+    $plainText = $beforePad .= $plainText.= $afterPad;
+
+    return $plainText;
+
+}
+
+sub rand_between {
+    my ($min, $max) = @_;
+    return $min + int(rand($max - $min))+1;
+}
 
 sub generate_aes_key {
-    my @set = ('0'..'9','A'..'F');
+    my @set = ('0'..'9','a'..'f');
     my $key = join '' => map $set[rand @set],1..16;
-    print "[key] $key\n";
+    return $key;
+}
+
+sub cbc_encrypt {
+    my ($plainText,$iv,$encrypt) = @_;
+    $plainText = pad($plainText,16);
+    my $result;
+    my $state = $iv;
+    for(my $offset = 0; $offset<length($plainText);$offset+=16){
+        my $block = substr($plainText,$offset,16);
+        $block = "$block" ^ "$state";
+        $state = $encrypt->encrypt($block);
+        $result .= $state;
+    }
+    return $result;
+}
+
+sub ecb_encrypt {
+    my ($plaintext,$cipher) = @_;
+    $plaintext = pad($plaintext, 16);
+    my $ciphertext = '';
+    for (my $offset = 0; $offset < length($plaintext); $offset += 16) {
+        my $block = substr($plaintext, $offset, 16);
+        $ciphertext .= $cipher->encrypt($block);
+    }
+    return $ciphertext;
+}
+
+sub pad {
+    my ($data, $blocklen) = @_;
+    my $mod = $blocklen - (length($data) % $blocklen);
+    $mod ||= $blocklen;
+    return $data . (chr($mod) x $mod);
 }
