8c0e0ac3394d99b7a6e80edcbbe34df0f9a56098
Will Cass <will_cass@hotmail.co.uk>
2018-11-10 01:02:36 +0000

    starting working on challenge 3

---
+++ perl/set2/c3.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+=begin
+An ECB/CBC detection oracle
+Now that you have ECB and CBC working:
+
+Write a function to generate a random AES key; that's just 16 random bytes.
+
+Write a function that encrypts data under an unknown key --- that is, a function that generates a random key and encrypts under it.
+
+The function should look like:
+
+encryption_oracle(your-input)
+=> [MEANINGLESS JIBBER JABBER]
+Under the hood, have the function append 5-10 bytes (count chosen randomly) before the plaintext and 5-10 bytes after the plaintext.
+
+Now, have the function choose to encrypt under ECB 1/2 the time, and under CBC the other half (just use random IVs each time for CBC). Use rand(2) to decide which to use.
+
+Detect the block cipher mode the function is using each time. You should end up with a piece of code that, pointed at a block box that might be encrypting ECB or CBC, tells you which one is happening.
+=cut
+use strict;
+use warnings;
+my $file = 'resources/bobross.txt';
+
+
+sub generate_aes_key {
+    my @set = ('0'..'9','A'..'F');
+    my $key = join '' => map $set[rand @set],1..16;
+    print "[key] $key\n";
+}
